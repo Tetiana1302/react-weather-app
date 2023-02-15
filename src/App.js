531f527a1e22774b797b865b9b220409d81d6b62
Tetiana1302 <116358640+Tetiana1302@users.noreply.github.com>
2023-02-15 11:12:44 +0000

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
+      <h1>Weather app </h1>
+      <footer>
+        This project is created by Tetiana Bysykalo and is{" "}
+        <a
+          href="https://github.com/Tetiana1302/react-weather-app"
+          target="_blank"
+        >
+          open-sorced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
