9dd993d053050cc53433df82d0d49bff4cebfd50
Tetiana1302 <116358640+Tetiana1302@users.noreply.github.com>
2023-02-16 12:55:46 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app </h1>
-      <footer>
-        This project is created by Tetiana Bysykalo and is{" "}
-        <a
-          href="https://github.com/Tetiana1302/react-weather-app"
-          target="_blank"
-        >
-          open-sorced on GitHub
-        </a>
-      </footer>
+      <div classname="container">
+        <h1>Weather app </h1>
+        <footer>
+          This project is created by Tetiana Bysykalo and is{" "}
+          <a
+            href="https://github.com/Tetiana1302/react-weather-app"
+            target="_blank"
+          >
+            open-sorced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
