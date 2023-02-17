df7ecd687199211046539cdc35a1b20724611e16
Tetiana1302 <116358640+Tetiana1302@users.noreply.github.com>
2023-02-17 09:31:56 +0000

    Added html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <div classname="container">
-        <h1>Weather app </h1>
+      <div className="container">
+        <Weather />
         <footer>
           This project is created by Tetiana Bysykalo and is{" "}
           <a
